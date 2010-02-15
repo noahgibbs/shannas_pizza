8835702aa568aaeef142ff81699aab79e21f1d5a
Noah Gibbs <noah_gibbs@yahoo.com>
2010-02-15 12:45:04 -0800

    Update TODO, build script

--- pizza.sh
+++ pizza.sh
@@ -3,4 +3,5 @@ SOURCE_FILES="pizza.c sprites.c"
 XYZ_FILES="xyz.c xyz_sprite.c xyz_events.c"
 ALL_SOURCE="$SOURCE_FILES $XYZ_FILES"
 LIBS="-lSDL_image -lSDL_ttf"
-gcc -Wall -o pizza $ALL_SOURCE -I. `sdl-config --cflags --libs` $LIBS
+OPTIONS="-Wall -O -g"
+gcc $OPTIONS -o pizza $ALL_SOURCE -I. `sdl-config --cflags --libs` $LIBS
