3128ad68404c4009a52507949bafdef3a79ddf44
Noah Gibbs <noah_gibbs@yahoo.com>
2010-02-23 21:53:43 -0800

    Update test code

--- test/test_conn.c
+++ test/test_conn.c
@@ -19,11 +19,23 @@ void assert(int cond, char *message) {
 
 /******* Tests ****************************/
 
-
-int main(void) {
+void connection1_test(void) {
   connector *conn;
 
   conn = new_connector(&type1, NULL);
+  assert(!!conn, "Couldn't allocate connection!");
+
+  assert(!!connector_new_input(conn), "Couldn't allocate input!");
+  assert(!!connector_new_output(conn), "Couldn't allocate output!");
+
+  destroy_connector(conn);
+}
+
+int main(void) {
+
+  init_connectors();
+  connection1_test();
+  shutdown_connectors();
 
   if(assertions_failed) {
     fprintf(stderr, "%d assertions failed!\n", assertions_failed);
