342060cb96658bdd699f0dbe146be08cbb6186c7
Noah Gibbs <noah_gibbs@yahoo.com>
2010-03-19 22:34:51 -0700

    Add SDL_gfx lib to link

--- build_pizza.sh
+++ build_pizza.sh
@@ -2,6 +2,6 @@
 SOURCE_FILES="pizza.c sprites.c connectors.c toppings.c gates.c"
 XYZ_FILES="xyz.c xyz_sprite.c xyz_events.c xyz_util.c"
 ALL_SOURCE="$SOURCE_FILES $XYZ_FILES"
-LIBS="-lSDL_image -lSDL_ttf"
+LIBS="-lSDL_image -lSDL_ttf -lSDL_gfx"
 OPTIONS="-Wall -g"
 gcc $OPTIONS -o pizza $ALL_SOURCE -I. `sdl-config --cflags --libs` $LIBS
