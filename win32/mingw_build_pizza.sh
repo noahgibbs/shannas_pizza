45ad109ad6433722fa37e99adafb43f6323c7adf
Noah Gibbs <noah_gibbs@yahoo.com>
2010-04-07 22:01:17 -0700

    Combine nearly-duplicate build scripts for Linux and MinGW

--- win32/mingw_build_pizza.sh
+++ win32/mingw_build_pizza.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd ..
-SOURCE_FILES="pizza.c sprites.c connectors.c toppings.c gates.c"
-XYZ_FILES="xyz.c xyz_sprite.c xyz_events.c xyz_util.c xyz_variable.c"
-ALL_SOURCE="$SOURCE_FILES $XYZ_FILES"
-LIBS="-lSDL_image -lSDL_ttf -lSDL_gfx"
-OPTIONS="-Wall -g"
-gcc $OPTIONS -o win32/pizza $ALL_SOURCE -I. -Lwin32/dlls `sdl-config --cflags --libs` $LIBS
+./build_pizza.sh
+mv pizza.exe win32/
