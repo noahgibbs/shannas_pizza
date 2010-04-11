aac0b7d55276684c86d0a1ab30333161c6556982
Noah Gibbs <noah_gibbs@yahoo.com>
2010-04-10 23:49:56 -0700

    Simple example use of animation system

--- simple_anim.c
+++ simple_anim.c
@@ -4,18 +4,17 @@
 #include "xyz_anim.h"
 
 double screen_color[3];
+xyz_anim *screen_background_anim = NULL;
 
 int screen_flash_eval(xyz_anim *anim) {
   double current_ratio = xyz_anim_get_current_ratio(anim);
-  fprintf(stderr, "Screen flash cur ratio: %g\n", current_ratio);
-  screen_color[0] = 0.0;
+  screen_color[0] = 255.0 * (1.0 - current_ratio);
   screen_color[1] = 0.0;
   screen_color[2] = 0.0;
   return 0;
 }
 
 int screen_flash_draw(xyz_anim *anim) {
-  fprintf(stderr, "Draw screen flash!\n");
   xyz_color((char)screen_color[0],
 	    (char)screen_color[1],
 	    (char)screen_color[2]);
@@ -23,9 +22,12 @@ int screen_flash_draw(xyz_anim *anim) {
   return 0;
 }
 
-xyz_anim *screen_background_anim = NULL;
+int screen_flash_delete(xyz_anim *anim) {
+  screen_background_anim = NULL;
+}
+
 xyz_anim_spec screen_background_spec = {
-  0, 750, 0, NULL, NULL, NULL, NULL, screen_flash_eval, screen_flash_draw
+  0, 250, 0, NULL, screen_flash_delete, NULL, NULL, screen_flash_eval, screen_flash_draw
 };
 
 void start_screen_flash(void) {
@@ -36,7 +38,8 @@ void start_screen_flash(void) {
     screen_background_anim = NULL;
   }
 
-  xyz_anim_create(&screen_background_spec, NULL);
+  screen_background_anim = xyz_anim_create(&screen_background_spec, NULL);
+  xyz_anim_start(screen_background_anim);
 }
 
 void screen_draw_background(void) {
