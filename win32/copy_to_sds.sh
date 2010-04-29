20b14cc032de54588312f18c0a64e71939ea5c9a
Noah Gibbs <noah_gibbs@yahoo.com>
2010-04-28 22:32:28 -0700

    Added automation script to deploy new version of Shanna's Pizza

---
+++ win32/copy_to_sds.sh
@@ -0,0 +1,5 @@
+cp shannas_pizza_demo_*.exe ../../../rails/shannasdaddy/public/versions/
+cd ../../../rails/shannasdaddy
+git add public/versions
+git commit -m "Added new version of Shanna's Pizza"
+git push origin master
